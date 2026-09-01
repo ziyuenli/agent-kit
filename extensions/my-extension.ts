cf946c7981f9e92c52c0a78e35f081ca709580e5
liziyuan <liziyuan199811@162.com>
2026-09-01 16:55:37 +0800

    chore: add pi extension package template

---
+++ extensions/my-extension.ts
@@ -0,0 +1,14 @@
+import type { ExtensionAPI, ExtensionCommandContext } from "@earendil-works/pi-coding-agent";
+
+export default function (pi: ExtensionAPI) {
+  pi.on("session_start", async (_event, ctx: ExtensionCommandContext) => {
+    ctx.ui.notify("my-extension 已加载", "info");
+  });
+
+  pi.registerCommand("hello", {
+    description: "回显一条问候",
+    handler: async (_args, ctx: ExtensionCommandContext) => {
+      ctx.ui.notify("Hello from ~/pi-extensions", "info");
+    },
+  });
+}
